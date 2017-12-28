57484246147b5bb9fd2196850926bb881334df3d
Johannes Akse <johannes.akse@knowit.no>
2017-12-28 12:30:12 +0100

    Adds PROJECTS route with db connection

--- august9-server/src/main/scala/ScalatraBootstrap.scala
+++ august9-server/src/main/scala/ScalatraBootstrap.scala
@@ -2,8 +2,13 @@ import com.august9.app._
 import org.scalatra._
 import javax.servlet.ServletContext
 
+import com.august9.app.controllers.ProjectController
+import org.json4s.{DefaultFormats, Formats}
+
 class ScalatraBootstrap extends LifeCycle {
+  implicit val formats: Formats = DefaultFormats
   override def init(context: ServletContext) {
-    context.mount(new August9ScalatraServlet, "/*")
+    //context.mount(new August9ScalatraServlet, "/*")
+    context.mount(new ProjectController, "/project/*")
   }
 }
--- august9-server/src/main/scala/com/august9/app/August9ScalatraServlet.scala
+++ august9-server/src/main/scala/com/august9/app/August9ScalatraServlet.scala
@@ -4,8 +4,5 @@ import org.scalatra._
 
 class August9ScalatraServlet extends ScalatraServlet {
 
-  get("/") {
-    views.html.hello()
-  }
 
 }
